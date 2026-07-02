499a1342b74b97c54160aa377ffe0c2b2ef1e809
Abhishek Sati <abhisheksati9876@gmail.com>
2026-07-02 10:02:15 +0200

    Removed packaging unit table from ZRA smat invoice

--- zra_smart_invoice/zra_smart_invoice/doctype/packaging_unit_of_measure/packaging_unit_of_measure.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Sudeep Panchpuri and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Packaging Unit Of Measure", {
-// 	refresh(frm) {
-
-// 	},
-// });
