77ba9182066c390cab2f3d4df3ae82fd0baca46d
Abhishek Sati <abhisheksati9876@gmail.com>
2026-07-11 17:39:09 +0200

    make sure TPIN is required in case of non-export customer

--- zra_smart_invoice/patches/customer/before_validate_hooks.py
+++ zra_smart_invoice/patches/customer/before_validate_hooks.py
@@ -1,6 +1,8 @@
 import frappe
+import re
 
 def before_validate(doc, method):
-    
-    if not doc.tax_id:
+    tax_category = doc.tax_category
+    tax_category =  re.sub(r"[^A-Za-z0-9]", "", tax_category).lower()
+    if not doc.tax_id and tax_category == "nonexport":
         frappe.throw("TPIN is required for Customer.")
