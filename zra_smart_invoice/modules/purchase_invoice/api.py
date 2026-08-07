1e8012cd4a1eba5de536135d06c3d1ee3b0dc8d2
Abhishek Sati <abhisheksati9876@gmail.com>
2026-08-07 12:30:36 +0200

    create save purchase sales API

--- zra_smart_invoice/modules/purchase_invoice/api.py
+++ zra_smart_invoice/modules/purchase_invoice/api.py
@@ -3,6 +3,7 @@ from zra_smart_invoice.api import get_item_type_name
 from zra_smart_invoice.client import make_vsdc_request
 import frappe
 from frappe import _
+from custom_api.utils.response import send_old_response, send_response_list
 
 @frappe.whitelist(allow_guest = True, methods=["GET"])
 def get_purchase_sales():
@@ -19,3 +20,24 @@ def get_purchase_sales():
     except Exception as e:
         raise e
 
+@frappe.whitelist(allow_guest = True, methods=["POST"])
+def save_purchase_sales():
+    try:
+        data = frappe.request.get_json()
+        payload = data
+        return send_response_list(
+                    status="success",
+                    message="Purchase Invoices saved successfully",
+                    data=None,
+                    status_code=200,
+                    http_status=200
+                )
+        # result = make_vsdc_request("trnsPurchase/saveTrnsPurchaseSales", payload)
+        # if result.get("resultCd") and result.get("resultCd") == "000":
+        #     return result
+
+        # else:
+        #     frappe.throw(_("Failed to save purchase sales to ZRA"))  
+          
+    except Exception as e:
+        raise e
