df0ff839fa0e32fa2adfe5d23e34972a47b14ec4
Abhishek Sati <abhisheksati9876@gmail.com>
2026-08-26 15:48:52 +0200

    added discount when Price > RRP

--- zra_smart_invoice/modules/mtv/utils.py
+++ zra_smart_invoice/modules/mtv/utils.py
@@ -13,7 +13,7 @@ def create_mtv_item_payload(item, item_doc, qty, rrp_rate, vat_cat_cd, tax_rate)
 
     #@TODO Discount is not Calculated for MTV Item need t calculate that
 
-    item_net_amt   = abs(round(float(item.net_rate or 0), 2))
+    item_net_amt   = abs(round(float(item.price_list_rate or 0), 2))
     item_vat_amt   = abs(round(item_net_amt * (tax_rate / 100), 4))
     item_unit_price   = abs(round(item_net_amt + item_vat_amt, 1))
 
@@ -23,7 +23,24 @@ def create_mtv_item_payload(item, item_doc, qty, rrp_rate, vat_cat_cd, tax_rate)
         tax_base_amount = item_net_amt
         vat_amt = item_vat_amt
 
+    prc = abs(round(item_net_amt+vat_amt, 2)) 
+
     totAmt = abs(round((item_net_amt+vat_amt)*qty, 2))
+    tax_base_amount = abs(round(tax_base_amount*flt(item.qty),4))
+    vat_amt = abs(round(vat_amt*flt(item.qty),4))
+
+    splyAmt = totAmt
+
+    discounted_net_price = None
+    discount_amount = 0
+    if item.discount_amount or item.discount_percentage:
+        discount_amount = abs(round(totAmt*(item.discount_percentage/100),4))
+        discounted_net_price = abs(round(totAmt-discount_amount, 4))
+        tax_base_amount = abs(round(discounted_net_price/(1+(tax_rate/100)),4))
+        vat_amt = abs(round(discounted_net_price-tax_base_amount,4))
+
+    if rrp_rate >= item_unit_price:
+        splyAmt = abs(round(rrp_rate * flt(item.qty), 2))
 
     payload =  {
             "itemSeq": item.idx,
@@ -36,20 +53,20 @@ def create_mtv_item_payload(item, item_doc, qty, rrp_rate, vat_cat_cd, tax_rate)
             "qtyUnitCd": frappe.get_value("UOM", item_doc.stock_uom, "common_code"),
             "qty": qty,
             "rrp": rrp_rate,
-            "prc": abs(round(item_net_amt+vat_amt, 2)),
-            "splyAmt": abs(round(rrp_rate * flt(item.qty), 2)) if rrp_rate >= item_unit_price else totAmt,            
+            "prc": prc,
+            "splyAmt": splyAmt,            
             "dcRt": item.discount_percentage,
-            "dcAmt": item.discount_amount,
+            "dcAmt": discount_amount,
             "isrccCd": "",
             "isrccNm": "",
             "isrcRt": 0,
             "isrcAmt": 0.0,
             "vatCatCd": vat_cat_cd.strip(),
             "exciseTxCatCd": None,
-            "vatTaxblAmt": abs(round(tax_base_amount*flt(item.qty),4)),
+            "vatTaxblAmt": tax_base_amount,
             "exciseTaxblAmt": 0.0,
-            "vatAmt": abs(round(vat_amt*flt(item.qty),4)),
+            "vatAmt": vat_amt,
             "exciseTxAmt": 0.0,
-            "totAmt": totAmt
+            "totAmt": totAmt if not item.discount_amount else discounted_net_price
         }
-    return payload, vat_amt*flt(item.qty), tax_base_amount*flt(item.qty)
+    return payload, vat_amt, tax_base_amount
