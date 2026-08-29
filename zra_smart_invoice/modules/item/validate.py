4b9d7cc25743af13867fd8cb54913971abdaf365
Abhishek Sati <abhisheksati9876@gmail.com>
2026-08-29 11:42:35 +0200

    optimized validation

--- zra_smart_invoice/modules/item/validate.py
+++ zra_smart_invoice/modules/item/validate.py
@@ -1,43 +1,16 @@
 import frappe
 
 def validate_zra_taxes(tax_templates):
-    vat_taxes = 0
-    insurance_premium_levy = 0
-    tourism_levy = 0
-    excise = 0
+    tax_categories = set()
+
     for tax_template in tax_templates:
         tax_template_category = tax_template.title.split("|")[-1].strip() if tax_template else "VAT"
         
-        if tax_template_category == "VAT":
-            if vat_taxes > 0:
-                frappe.throw("Multiple VAT taxes are not allowed on the same item. Please keep only one VAT tax.")
-            else:
-                vat_taxes +=1
+        if tax_template_category in tax_categories:
+            frappe.throw(
+                f"Only one '{tax_template_category}' tax is allowed on an item. "
+                f"Please remove the duplicate tax."
+            )
 
-        elif tax_template_category == "Insurance Premium Levy":
-            if insurance_premium_levy > 0:
-                frappe.throw(
-                            "Multiple Insurance Premium Levy taxes are not allowed on the same item. "
-                            "Please keep only one Insurance Premium Levy tax."
-                        )            
-            else:
-                insurance_premium_levy +=1
-
-        elif tourism_levy == "Tourism levy":
-            if tourism_levy > 0:
-                frappe.throw(
-                            "Multiple Tourism Levy taxes are not allowed on the same item. "
-                            "Please keep only one Tourism Levy tax."
-                        )
-            else:
-                tourism_levy +=1
-
-        elif excise == "Excise":
-            if excise > 0:
-                frappe.throw(
-                            "Multiple Excise taxes are not allowed on the same item. "
-                            "Please keep only one Excise tax."
-                        )
-            else:
-                excise += 1
+        tax_categories.add(tax_template_category)
             
